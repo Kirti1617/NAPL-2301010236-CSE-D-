903148a9a6d43eaf6b8d0616b070bd8b0667a99e
Kirti1617 <kirtisahni1617@gmail.com>
2026-09-08 12:04:57 +0530

    Lab 2 uploaded

--- Lab 2/main.go
+++ Lab 2/main.go
@@ -1,6 +1,20 @@
 package main
-import"fmt"
-import "example.com/app/mathutil" 
+
+import (
+	"fmt"
+
+	"lab2/mathutil"
+	"lab2/strop"
+)
+
 func main() {
-	fmt.Println(mathutil.Add(5,3))
+
+	// String operations
+	text := "Hello World"
+	fmt.Println("Original String :", text)
+	fmt.Println("Reversed String :", strop.Reverse(text))
+	fmt.Println("Number of Vowels:", strop.CountVowels(text))
+	// Mathematical operations
+	fmt.Println("Factorial of 5  :", mathutil.Factorial(5))
+	fmt.Println("2 raised to 5   :", mathutil.Power(2, 5))
 }
