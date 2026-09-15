246e889cd2c5b2a4958b828bf62de86a41e99e8c
Kirti1617 <kirtisahni1617@gmail.com>
2026-09-15 12:08:45 +0530

    Added exp3.go

--- Lab 2/main.go
+++ Lab 2/main.go
@@ -1,20 +1,13 @@
 package main
-
-import (
-	"fmt"
-
-	"lab2/mathutil"
-	"lab2/strop"
-)
-
 func main() {
 
-	// String operations
-	text := "Hello World"
-	fmt.Println("Original String :", text)
-	fmt.Println("Reversed String :", strop.Reverse(text))
-	fmt.Println("Number of Vowels:", strop.CountVowels(text))
-	// Mathematical operations
-	fmt.Println("Factorial of 5  :", mathutil.Factorial(5))
-	fmt.Println("2 raised to 5   :", mathutil.Power(2, 5))
+	sliceOperations()
+	mapOperations()
 }
+// 	text := "Hello World"
+// 	fmt.Println("Original String :", text)
+// 	fmt.Println("Reversed String :", strop.Reverse(text))
+// 	fmt.Println("Number of Vowels:", strop.CountVowels(text))
+// 	fmt.Println("Factorial of 5  :", mathutil.Factorial(5))
+// 	fmt.Println("2 raised to 5   :", mathutil.Power(2, 5))
+// }
